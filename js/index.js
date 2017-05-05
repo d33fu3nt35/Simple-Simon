9437bff27919265463ab123bf8e832f6c9163fac
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-05-04 19:14:48 -0500

    I changed the particle background with PARTICLE JS, also tried to fix the auto typing animation on the index, but that didn't go so well.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
-function autoType(elementClass, typingSpeed) {
+function autoType(elementClass, typingSpeed){
     var thhis = $(elementClass);
     thhis.css({
         "position": "relative",
         "display": "inline-block"
     });
-    thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
     thhis = thhis.find(".text-js");
     var text = thhis.text().trim().split('');
     var amntOfChars = text.length;
     var newString = "";
     thhis.text("|");
-    setTimeout(function () {
-        thhis.css("opacity", 1);
+    setTimeout(function(){
+        thhis.css("opacity",1);
         thhis.prev().removeAttr("style");
         thhis.text("");
-        for (var i = 0; i < amntOfChars; i++) {
-            (function (i, char) {
-                setTimeout(function () {
+        for(var i = 0; i < amntOfChars; i++){
+            (function(i,char){
+                setTimeout(function() {
                     newString += char;
                     thhis.text(newString);
-                }, i * typingSpeed);
-            })(i + 1, text[i]);
+                },i*typingSpeed);
+            })(i+1,text[i]);
         }
-    }, 1000);
+    },1500);
 }
 
-$(document).ready(function () {
-    autoType(".type-js", 500);
+$(document).ready(function(){
+    autoType(".type-js",200);
 
 
 });
